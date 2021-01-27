2de15bc0f0fc463beb0044d0532fa6c3640ff959
juanma1980 <juanma1980@gmail.com>
2021-01-27 19:13:37 +0100

    update to python3

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ from StringIO import StringIO
 from ConfigParser import SafeConfigParser
 
 data = StringIO('\n'.join(line.strip() for line in open('/etc/samba/smb.conf')))
-print data
+print (data)
 parser = SafeConfigParser()
 parser.readfp(data)
 
